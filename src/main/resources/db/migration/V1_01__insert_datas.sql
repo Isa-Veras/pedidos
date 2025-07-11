32c27067c0ccc9a5ee7fe8ab4a9d03e1adaff1b0
Isabella Souza <isabella.souza@greatpeople.com.br>
2025-07-10 23:30:10 -0300

    ajuste banco de dados

--- src/main/resources/db/migration/V1_01__insert_datas.sql
+++ src/main/resources/db/migration/V1_01__insert_datas.sql
@@ -1,28 +1,28 @@
 -- CLIENTE
 INSERT INTO cliente (id_cliente, nome, documento)
-VALUES (3, 'Julia Batista', 12345678901);
+VALUES (10, 'Julia Batista', 12345678901);
 INSERT INTO cliente (id_cliente, nome, documento)
-VALUES (2, 'João da Silva', 47695238214);
+VALUES (20, 'João da Silva', 47695238214);
 
 
 -- PEDIDO
 INSERT INTO pedido (id_pedido, code, id_cliente, status, data)
-VALUES (4, 1004, 1, 'processed', now());
+VALUES (10, 1001, 10, 'processed', now());
 INSERT INTO pedido (id_pedido, code, id_cliente, status, data)
-VALUES (2, 1002, 2, 'processing', now());
+VALUES (20, 1002, 20, 'processing', now());
 INSERT INTO pedido (id_pedido, code, id_cliente, status, data)
-VALUES (3, 1003, 2, 'new', now());
+VALUES (30, 1003, 20, 'new', now());
 
 -- ITENS
-INSERT INTO item (quantidade, nome, id_cliente, id_pedido, preco)
+INSERT INTO item (id_item, quantidade, nome, id_cliente, id_pedido, preco)
 VALUES
-  (100, 'lápis', 1, 1, 1.10),
-  (10, 'caderno', 1, 1, 1.00);
-INSERT INTO item (quantidade, nome, id_cliente, id_pedido, preco)
+  (20,100, 'lápis', 10, 10, 1.10),
+  (22,10, 'caderno', 10, 10, 1.00);
+INSERT INTO item (id_item, quantidade, nome, id_cliente, id_pedido, preco)
 VALUES
-(5, 'mochila', 2, 2, 55.20),
-(2, 'notebook', 2, 2, 2256.99);
-INSERT INTO item (quantidade, nome, id_cliente, id_pedido, preco)
+(21,5, 'mochila', 20, 20, 55.20),
+(23,2, 'notebook', 20, 20, 2256.99);
+INSERT INTO item (id_item, quantidade, nome, id_cliente, id_pedido, preco)
 VALUES
-(2, 'livro', 2, 3, 25),
-(1, 'caneta', 2, 3, 5.20);
+(24,2, 'livro', 20, 30, 25),
+(25,1, 'caneta', 20, 30, 5.20);
